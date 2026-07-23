b67d06a0ac8a7fa61abe500a772595a747125ff2
ithasaimahitha-lab <ithasaimahitha@gmail.com>
2026-07-23 19:09:49 +0530

    Delete deepskilling/Week5/WEEK-5 directory

--- deepskilling/Week5/WEEK-5/src/app/store/reducers/app.state.ts
+++
@@ -1,7 +0,0 @@
-import { StudentState } from './student.reducer';
-import { CourseState } from './course.reducer';
-
-export interface AppState {
-  students: StudentState;
-  courses: CourseState;
-}
