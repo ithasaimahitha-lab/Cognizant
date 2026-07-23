05a6fd25dae7318287840fe25cb34fcc3f97eb84
ithasaimahitha-lab <ithasaimahitha@gmail.com>
2026-07-23 12:05:35 +0530

    Delete deepskilling/Week 5 directory

--- deepskilling/Week 5/student-course-portal/src/app/guards/unsaved-changes.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { ReactiveEnrollmentFormComponent } from '../features/enrollment/reactive-enrollment-form/reactive-enrollment-form';
-
-export const unsavedChangesGuard: CanDeactivateFn<ReactiveEnrollmentFormComponent> = (component) => {
-  if (component.enrollForm.dirty && !component.submitted) {
-    return window.confirm('You have unsaved changes. Leave?');
-  }
-  return true;
-};
--- deepskilling/Week 5/student-course-portal/src/app/services/auth.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class AuthService {
-  // Mock login status for Hands-On 7
-  isLoggedIn = true;
-}
